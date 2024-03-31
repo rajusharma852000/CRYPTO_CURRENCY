caa68c35dba116b0579fae7f92e8c486d2e2c934
Raju Sharma <raju.sharma@iitg.ac.in>
2024-03-31 21:50:58 +0530

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -1,35 +1,52 @@
+//To get the corresponding codes of cryptocurrencies
+//The following codes are used in API to fetch the data of different cryptocurrencies
 let currencyCode = {
-    "BTC" : ["Bitcoin", 'images/bitcoin.png'],
-    "ETH" : ["Ethereum", 'images/ethereum.png'],
-    "BNB" : ["Binance Coin", 'images/binancecoin.png'],
-    "ADA" : ["Cardano", 'images/cardano.png'],
-    "SOL" : ["Solana", 'images/solana.png'],
-    "XRP" : ["Ripple", 'images/ripple.png'],
-    "DOT" : ["Polkadot", 'images/polkadot.png'],
-    "DOGE": ["Doge Coin", 'images/dogecoin.png'],
-    "AVAX": ["Avalanche", 'images/avalanche.png'],
-    "LUNA": ["Terra", 'images/terra.png'],
-    "MATIC":[ "Polygon", 'images/polygon.png'],
-    "LTC" : ["Litecoin", 'images/litecoin.png'],
-    "BCH" : ["Bitcoin Cash", 'images/bitcoincash.png'],
-    "LINK": ["Chainlink", 'images/chainlink.png'],
-    "ALGO": ["Algorand", 'images/algorand.png'],
-    "UNI" : ["Uniswap", 'images/uniswap.png'],
-    "TRX" : ["Tron", 'images/tron.png'],
-    "MANA": ["Decentraland", 'images/decentraland.png'],
-    "ATOM": ["Cosmos", 'images/cosmos.png'],
+    "Bitcoin"          : ["BTC"  , 'images/bitcoin.png'],
+    "Ethereum"         : ["ETH"  , 'images/ethereum.png'],
+    "Binancecoin"     : ["BNB"  , 'images/binancecoin.png'],
+    "Cardano"          : ["ADA"  , 'images/cardano.png'],
+    "Solana"           : ["SOL"  , 'images/solana.png'],
+    "Ripple"           : ["XRP"  , 'images/ripple.png'],
+    "Polkadot"         : ["DOT"  , 'images/polkadot.png'],
+    "Dogecoin"        : ["DOGE" , 'images/dogecoin.png'],
+    "Avalanche"        : ["AVAX" , 'images/avalanche.png'],
+    "Terra"            : ["LUNA" , 'images/terra.png'],
+    "Polygon"          : ["MATIC", 'images/polygon.png'],
+    "Litecoin"         : ["LTC"  , 'images/litecoin.png'],
+    "Bitcoincash"     : ["BCH"  , 'images/bitcoincash.png'],
+    "Chainlink"        : ["LINK" , 'images/chainlink.png'],
+    "Algorand"         : ["ALGO" , 'images/algorand.png'],
+    "Uniswap"          : ["UNI"  , 'images/uniswap.png'],
+    "Tron"             : ["TRX"  , 'images/tron.png'],
+    "Decentraland"     : ["MANA" , 'images/decentraland.png'],
+    "Cosmos"           : ["ATOM" , 'images/cosmos.png'],
 }
-const savedCodes = [];
 
-const is_match = (code) =>{
+
+
+//Crypto Saved By User In Watchlist
+const savedCurrencyNames = [];
+
+
+
+
+
+//Check if the CryptoName Entered by User is Correct
+const is_match = (currencyName) =>{
     for(let key in currencyCode){
-        if(key === code){
+        if(key === currencyName){
             return 1;
         }
     }
     return 0;
 }
-function addToList(code){
+
+
+
+
+
+//Prototype of list elements
+function addToList(currencyName){
     let divElement = document.createElement('div');
     divElement.classList.add('watchlist-crypto-box');
     let parent = document.getElementsByClassName('watchlist-main-box')[0];
@@ -49,48 +66,93 @@ function addToList(code){
     img.removeAttribute('id');
     span.removeAttribute('id');
     h3.removeAttribute('id');
-    img.setAttribute('src', currencyCode[code][1]);
-    img.setAttribute('alt', currencyCode[code][0]);
-    span.textContent = currencyCode[code][0];
-    h3.setAttribute('id', 'id-' + code);
-    i.setAttribute('id', 'i-id-' + code);
+    img.setAttribute('src', currencyCode[currencyName][1]);
+    img.setAttribute('alt', currencyName);
+    span.textContent = currencyName;
+    h3.setAttribute('id', 'id-' + currencyCode[currencyName][0]);
+    i.setAttribute('id', 'i-id-' + currencyCode[currencyName][0]);
 }
 
-function addItem(code){
-    savedCodes.push(code);
-    addToList(code);
 
-    document.querySelector(`#i-id-`+code).addEventListener('click', ()=>{
-        localStorage.removeItem(code);
-        for(let i = 0; i < savedCodes.length; i++){
-            if(savedCodes[i] === code){
-                savedCodes.splice(i, 1);
+
+
+
+//Add Item to the list
+function addItem(currencyName){
+    savedCurrencyNames.push(currencyName);
+    addToList(currencyName);
+
+    //event listener to remove an element form the list 
+    document.querySelector(`#i-id-`+currencyCode[currencyName][0]).addEventListener('click', ()=>{
+        localStorage.removeItem(currencyName);
+        for(let i = 0; i < savedCurrencyNames.length; i++){
+            if(savedCurrencyNames[i] === currencyName){
+                savedCurrencyNames.splice(i, 1);
             }
         }
-        let ElementNode = document.getElementById('id-'+code);
+        let ElementNode = document.getElementById('id-'+currencyCode[currencyName][0]);
         let parentElement = ElementNode.parentElement;
         parentElement.remove();
     })
 }
+
+
+
+
+
+//Make the code works even if the user input consists 
+//1. Extra white spaces
+//2. All small letters
+//3. All capital letters
+//4. combinaton of small and capitals
+function formatString(inputString) {
+    // Split the string into words
+    const words = inputString.split(' ');
+
+    // Remove leading and trailing whitespace from each word, convert to lowercase
+    const formattedWords = words.map(word => word.trim().toLowerCase());
+
+    // Join the formatted words into a single string
+    const formattedString = formattedWords.join('');
+
+    // Capitalize the first letter of the formatted string
+    const firstLetterCapitalized = formattedString.charAt(0).toUpperCase() + formattedString.slice(1);
+
+    return firstLetterCapitalized;
+}
+
+
+
+
+
+
+//Event listener to be excecuted on click
 let input = document.getElementById('addToWatchlist-Subdiv').firstElementChild;
 let button = input.nextElementSibling;
 button.addEventListener('click', ()=>{
-    let code = input.value;
-    if(!is_match(code)){
+    let currencyName = formatString(input.value);
+    if(!is_match(currencyName)){
         input.value = "Invalid code !";
         return;
     }
     input.value = "";
-    localStorage.setItem(code, 'cryptoCurrency');
-    addItem(code);
+    localStorage.setItem(currencyName, 'cryptoCurrency');
+    addItem(currencyName);
 })
 
+
+
+
+//API URL
 let halfURL = 'https://api.binance.com/api/v3/ticker/price?symbol=';
+
+//Fetch the Price Every Second to keep the user Updated
 let intervalForCryptoId = setInterval(() => {
     async function fetchPrice() {
-        for (let i = 0; i < savedCodes.length; i++) {
-            let url = halfURL + savedCodes[i] + "USDT";
-            let newId = document.getElementById('id-' + savedCodes[i]);
+        for (let i = 0; i < savedCurrencyNames.length; i++) {
+            let code = currencyCode[savedCurrencyNames[i]][0];
+            let url = halfURL + code + "USDT";
+            let newId = document.getElementById('id-' + code);
             let p = await fetch(url);
             let res = await p.json();
             if(newId != null) newId.innerHTML = "$" + parseFloat(res.price).toFixed(4) ;
@@ -99,6 +161,25 @@ let intervalForCryptoId = setInterval(() => {
     fetchPrice();
 }, 1000);
 
+
+
+//to delete the Watchlist
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the clear button element
+    const clearButton = document.getElementById("watchlist-delete");
+
+    // Add click event listener to the clear button
+    clearButton.addEventListener("click", function() {
+        // Clear local storage
+        localStorage.clear();
+        // Reload the page
+        location.reload();
+    });
+});
+
+
+
+//Store the Wathlist in local storage, so that the list won't vanish on reload
 (function () {
     for (let x in localStorage) {
         let value = localStorage.getItem(x);
@@ -107,9 +188,6 @@ let intervalForCryptoId = setInterval(() => {
         }
     }
 })();
-
-
-
 
 
 
